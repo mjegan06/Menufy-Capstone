bdf3fc52d0bff9f1394e644bcf9dd1da03d29404
Serena Tay <tays@oregonstate.edu>
2020-04-18 21:09:28 -0700

    test

--- application.py
+++ application.py
@@ -43,8 +43,7 @@ def menuPage():
 		FilterExpression=Attr('menu_id').eq('menu_1')
 	)
 	data = json.dumps(response['Items'], cls=DecimalEncoder)
-	for each in data:
-		print(json.dumps(each))
+	print(data)
 
 	return render_template('menu.html', response=data)
 
