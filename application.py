38b9afc2840eb8e14e21abeaf6a90dd1a79c027c
adrath <dratha@oregonstate.edu>
2020-04-19 20:44:41 -0500

    update to login accounts

--- application.py
+++ application.py
@@ -130,7 +130,7 @@ def signup(customer_username, customer_id):
             FilterExpression=Attr('customer_username').eq(customer_username)
         )
 
-        if not result['customer_username']:
+        if not result['Items']:
             flash("The username already exists. Please enter another username.", "danger")
             return render_template('signup.html')
 
