664db74aee53afa4b5780783ba9ae256b7f809b0
adrath <dratha@oregonstate.edu>
2020-04-30 19:57:56 -0500

    Order History added

--- order.py
+++ order.py
@@ -41,7 +41,7 @@ def get_order(customer_username, customer_id, restaurant_id):
 
     #get the current time and convert it into a string
     named_tuple = time.localtime() # get struct_time
-    time_string = time.strftime("%m/%d/%Y, %H:%M:%S", named_tuple)
+    time_string = time.strftime("%Y/%m/%d, %H:%M:%S", named_tuple)
 
     res = dict(zip(menu_items, item_quantity))
     print(res)
@@ -67,7 +67,68 @@ def get_order(customer_username, customer_id, restaurant_id):
     #print(string_to_time)
 
     return render_template('order.html', customer_username=customer_username, customer_id=customer_id, restaurant_id=restaurant_id, order=order)
+
+
+
+@bp.route('/<restaurant_id>/order_history', methods=['GET','POST'])
+@check_user_login
+def get_order_history(customer_username, customer_id, restaurant_id):
+
+    restaurant_table=dynamodb.Table('restaurant')
+    order_table=dynamodb.Table('order')
+
+    restaurant_data = restaurant_table.query(
+        KeyConditionExpression=Key('restaurant_id').eq(restaurant_id)
+    )
+    restaurant_name = restaurant_data['Items'][0]['restaurant_name']
+
+    if request.method == 'POST':
+        order_date = request.form['order_date'] if request.form['order_date'] else None
+        order_time = request.form['order_time'] if request.form['order_time'] else None
+        end_date = request.form['end_date'] if request.form['end_date'] else None
+        end_time = request.form['end_time'] if request.form['end_time'] else None
+
+        if order_date and end_date:
+            print(order_date)
+            print(end_date)
+            order_date_string = order_date #time.strftime("%m/%d/%Y", order_date)
+            end_date_string = end_date #time.strftime("%m/%d/%Y", end_date)
+            if order_time:
+                order_time_string = time.strftime("%H:%M:%S", order_time)
+            else:
+                order_time_string = "00:00:00"
+            
+            if end_time:
+                end_time_string = time.strftime("%H:%M:%S", end_time)
+            else:
+                end_time_string = "00:00:00"
+            
+            order_date_time = "%s, %s" % (order_date_string, order_time_string)
+            end_date_time = "%s, %s" % (end_date_string, end_time_string)
+
+            print(order_date_time)
+            print(end_date_time)
+
+            order_data = order_table.scan(
+                FilterExpression=Attr('restaurant_id').eq(restaurant_id) # & Attr('order_date').between(order_date_time, end_date_time)
+            )
+
+            print(order_data)
+
+
+            return render_template('order_history.html', customer_username=customer_username, customer_id=customer_id, restaurant_id=restaurant_id, orders=order_data)
+
+    order = None
     
+    #convert the string back into time
+    #string_to_time = time.strptime(time_string, "%m/%d/%Y, %H:%M:%S")
+    #print(string_to_time)
+
+    return render_template('order_history.html', customer_username=customer_username, customer_id=customer_id, restaurant_id=restaurant_id, orders=order)
+
+
+
+
     # # get restaurant name
     # restaurant_table=dynamodb.Table('restaurant') 
     # restaurant_data = restaurant_table.scan(
